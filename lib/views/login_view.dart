d70ec1a80d7e3ef27b156009f0d9513bfa76a6e3
Arpit-Mohapatra007 <arpitmohapatra06@gmail.com>
2025-05-22 20:00:23 +0530

    Step-9

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/show_error_dialog.dart';
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -58,33 +59,35 @@ class _LoginViewState extends State<LoginView> {
                         final email=_email.text;
                         final password=_password.text;
                         try{
-                         await FirebaseAuth.instance.signInWithEmailAndPassword(
+                         await AuthService.firebase().logIn(
                           email: email, 
                           password: password
                           );
-                          final user = FirebaseAuth.instance.currentUser;
-                          if (user?.emailVerified??false) {
+                          final user = AuthService.firebase().currentUser;
+                          if (user?.isEmailVerified??false) {
                             Navigator.of(context).pushNamedAndRemoveUntil(
                             notesRoute,
                             (route) => false,
                             );
                           }else{
-                            await user?.sendEmailVerification();
+                            AuthService.firebase().sendEmailVerification();
                             Navigator.of(context).pushNamedAndRemoveUntil(
                             verifyEmailRoute,
                             (route) => false,
                             );
                           }
-                      } on FirebaseAuthException catch (e) {
-                        if(e.code=='user-not-find'){
-                          await showErrorDialog(context, 'Error:User Not Found !!',);
-                        }else if(e.code=='wrong-password'){
-                          await showErrorDialog(context, 'Error:Wrong password !!',);
-                        }else{
-                          await showErrorDialog(context, 'Error:${e.code}',);
-                        }
-                      } catch (e){
-                        await showErrorDialog(context, 'Error:${e.toString()}',);
+                      } on UserNotFoundAuthException{
+                         await showErrorDialog(
+                          context, 
+                          'Error:User Not Found !!',);
+                      } on WrongPasswordAuthException{
+                          await showErrorDialog(
+                            context, 
+                            'Error:Wrong password !!',);
+                      } on GenericAuthException{
+                          await showErrorDialog(
+                            context, 
+                            'Error:Authentication Error !!',);
                       }
                       },
                       child: const Text('Login'),
--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -1,6 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -22,13 +22,12 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
             const Text("We've sent you an email verification, Please verify your account."),
             const Text("If you haven't recieved a verification email yet press below:"),
             TextButton(onPressed: () async {
-              final user =FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
+              await AuthService.firebase().sendEmailVerification();
             }, 
             child: const Text('Resend email verification'),),
             TextButton(
               onPressed: () async {
-               await FirebaseAuth.instance.signOut();
+               await AuthService.firebase().logOut();
                Navigator.of(context).pushNamedAndRemoveUntil(
                 registerRoute, 
                 (route)=>false);
