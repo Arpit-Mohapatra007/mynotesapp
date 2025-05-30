d4f46bc6cda2a5c60e99a89ccc4c431c9fbb7379
Arpit-Mohapatra007 <arpitmohapatra06@gmail.com>
2025-05-30 15:34:54 +0530

    Step-18

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes/create_update_notes_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'My Notes',
       theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 4, 0)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 4, 0),
+        ),
       ),
-      home: const HomePage(),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const HomePage(),
+      ),
       routes: {
-        loginRoute:(context)=> const LoginView(),
-        registerRoute:(context)=> const RegisterView(),
-        notesRoute:(context)=>const NotesView(),
-        verifyEmailRoute:(context)=>const VerifyEmailView(),
-        createUpdateNotesRoute:(context)=>const CreateUpdateNoteView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
+        createUpdateNotesRoute: (context) => const CreateUpdateNoteView(),
       },
-    ),);
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-          future:AuthService.firebase().initialize(),
-          builder:(context, snapshot){
-            switch(snapshot.connectionState){
-              case ConnectionState.done:
-              final user = AuthService.firebase().currentUser;
-              if(user!=null){
-                if (user.isEmailVerified) {
-                  return const NotesView();
-                }else{
-                  return const VerifyEmailView();
-                }
-              }else{
-                return const LoginView();
-              }
-                default:
-                  return const CircularProgressIndicator();
-        }
-
-          },
+    context.read<AuthBloc>().add(const AuthEventIntialize());
+    return BlocBuilder<AuthBloc, AuthState>(builder: (context,state){
+      if(state is AuthStateLoggedIn){
+        return const NotesView();
+      }else if(state is AuthStateNeedsVerification){
+        return const VerifyEmailView();
+      } else if(state is AuthStateLoggedOut){
+        return const LoginView();
+      }else{
+        return const Scaffold(
+          body: CircularProgressIndicator(),
         );
+      }
+    });
   }
-
 }
-
-
